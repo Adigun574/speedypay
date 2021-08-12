3916575eb2b1e7b3c9ed403eecb65079e5240e34
Adigun Ibrahim <adigunibrahim574@gmail.com>
2021-08-12 12:39:36 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { MainComponent } from './components/main/main.component';
-import { PropertiesComponent } from './components/properties/properties.component';
-import { PropertyDetailsComponent } from './components/property-details/property-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent},
-  {path:'main',component:MainComponent,
-    children:[
-      {
-        path:'properties',
-        component:PropertiesComponent,
-        // canActivate:[AuthGuard]
-      },
-      {
-        path:'properties/:id',
-        component:PropertyDetailsComponent,
-        // canActivate:[AuthGuard]
-      }
-    ]
-  },
-  {path:'', redirectTo:'login', pathMatch:'full'},
-  {path:'**', component:LoginComponent}
+  {
+    path:'',
+    component:HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LoaderComponent } from './components/loader/loader.component';
-import { MainComponent } from './components/main/main.component';
-import { PropertiesComponent } from './components/properties/properties.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { PropertyDetailsComponent } from './components/property-details/property-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    LoaderComponent,
-    MainComponent,
-    PropertiesComponent,
-    PropertyDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +18,6 @@ import { PropertyDetailsComponent } from './components/property-details/property
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
